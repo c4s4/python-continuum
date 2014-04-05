9dca488486b17c7f9d9ef52133d6a848b3271139
Michel Casabianca <michel.casabianca@gmail.com>
2014-04-05 13:49:21 +0200

    Added config

--- src/continuum.py
+++ src/continuum.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # encoding: UTF-8
 
+import yaml
+
+
 class Continuum(object):
 
-    def main(self):
+    def __init__(self, config):
+        with open(config) as stream:
+            self.config = yaml.load(stream)
+
+    def run(self):
         pass
 
 
 if __name__ == '__main__':
-    Continuum().main()
-
+    for config in sys.argv[1:]:
+        Continuum(config).run()
--- test/test_continuum.py
+++ test/test_continuum.py
@@ -9,8 +9,11 @@ import continuum
 
 class Test(unittest.TestCase):
 
+    TEST_DIR = os.path.dirname(__file__)
+    CONFIG = os.path.join(TEST_DIR, 'config.yml')
+
     def test(self):
-        continuum.Continuum().main()
+        continuum.Continuum(self.CONFIG).run()
 
 
 if __name__ == '__main__':
