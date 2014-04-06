cffc10ad41108c5df628ceefeb13b75a845ca9c2
Michel Casabianca <casa@sweetohm.net>
2014-04-06 23:19:06 +0200

    Implementation

--- src/continuum.py
+++ src/continuum.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import yaml
+import shutil
 
 
 class Continuum(object):
@@ -20,22 +21,36 @@ class Continuum(object):
         for module in modules.keys():
             builds[module] = self.build(name=module,
                                         module=modules[module])
+        print builds
     
     def build(self, name, module):
-        url = module['url']
-        command = module['command']
-        report = {}
+        report = {'name': name}
         current_dir = os.getcwd()
         try:
             os.chdir(self.config['directory'])
-            self.execute_with_output("")
+            command = ['git', 'clone', module['url'], name]
+            report['output'] = '## CHECKOUT ##\n'
+            report['output'] += self.execute_with_output(command)
+            module_dir = os.path.join(self.config['directory'], name)
+            module_git = os.path.join(module_dir, '.git')
+            os.chdir(module_dir)
+            shutil.rmtree(module_git)
+            report['output'] += '\n## BUILD ##\n'
+            report['output'] += self.execute_with_output(module['command'], shell=True)
             report['error'] = False
-        except Exception:
+        except subprocess.CallProcessError, e:
             report['error'] = True
+            report['output'] += e.output
         finally:
             os.chdir(current_dir)
+        return report
         
-
+    @staticmethod
+    def execute_with_output(command, stdin=None, shell=False):
+        if stdin:
+            return subprocess.check_output(command, stderr=subprocess.STDOUT, stdin=stdin, shell=shell)
+        else:
+            return subprocess.check_output(command, stderr=subprocess.STDOUT)
 
 if __name__ == '__main__':
     for _config in sys.argv[1:]:
